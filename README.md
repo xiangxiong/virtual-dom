2066540b2f5b78b67c8f503a82fec5123ec473fb
shawn <xx090304@163.com>
2018-09-22 13:51:21 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,66 @@
-## 生成项目
+#1、virtual dom
+定义: vitual dom, 也就是虚拟节点. 它通过JS的Object对象模拟的Dom节点,然后再通过特定的render方法将其渲染成真实的Dom节点.
 
+## 1.1 实现的思路
+- 1.用Javascript 对象结构表示DOM树的结构; 然后用这个树构建一个真正的DOM树,插入到文档当中.
+- 2.当状态变更的时候，重新构造一颗新的对象树。然后用新的树和旧的树进行比较，记录两颗树的差异.
+- 3.把2所记录的差异应用到步骤1所构建的真正的DOM树上,视图就更新了.
 
-## virtual dom
+## 1.2 实现的过程
+
+- 1.用Javascript 对象结构表示DOM树的结构; 然后用这个树构建一个真正的DOM树,插入到文档当中.
+
+``` element.js
+
+class Element{
+    constructor(type,props,children){
+        this.type = type;
+        this.props = props;
+        this.children = children;
+    }
+}
+
+function createElement(type,props,children){
+    return new Element(type,props,children);
+}
+
+function setAttr(node,key,value){
+    switch(key){
+        case 'value':
+            if(node.tagName.toUpperCase() === 'INPUT'||
+            node.tagName.toUpperCase() === 'TEXTAREA'){
+                node.value = value;
+            }else{
+                node.setAttribute(key,value);
+            }
+        break;
+        case 'style':
+            node.style.cssText = value;
+        break;
+        default:
+            node.setAttribute(key,value);
+            break;
+    }
+}
+
+// render 方法可以将vnode转换成真实dom.
+function render(eleObj){
+    let el = document.createElement(eleObj.type);
+    for(let key in eleObj.props){
+        // 设置属性的方法
+        setAttr(el,key,eleObj.props[key]);
+    }
+    eleObj.children.forEach(child=>{
+        child = (child instanceof Element)?
+        render(child):document.createTextNode(child);
+        el.appendChild(child);
+    });
+    return el;
+}
+
+function renderDom(el,target){
+    target.appendChild(el);
+}
+
+export {createElement,render,renderDom};
+```
--- src/element.js
+++ src/element.js
@@ -1,59 +1,55 @@
-// 虚拟Dom元素的类.
+
 class Element{
-  constructor(type,props,children){
-    this.type = type;
-    this.props = props;
-    this.children = children;
-  }
+    constructor(type,props,children){
+        this.type = type;
+        this.props = props;
+        this.children = children;
+    }
 }
 
-// 设置属性.
-function setAttr(node,key,value){
-  switch (key) {
-      case 'value':  // node是一个input或者textarea
-          if(node.tagName.toUpperCase() === 'INPUT' ||
-            node.tagName.toUpperCase() === 'TEXTAREA'){
-            node.value = value;
-          }else{
-            node.setAttribute(key,value);
-          }
-      break;
-      case 'style':
-        node.style.cssText = value;
-      break;
-    default:
-        node.setAttribute(key,value);
-      break;
-  }
-}
-
-// 返回虚拟节点的 返回对象.
 function createElement(type,props,children){
-  return new Element(type,props,children);
+    return new Element(type,props,children);
 }
 
-// render方法将虚拟dom转换成真实dom
+function setAttr(node,key,value){
+    switch(key){
+        case 'value':
+            if(node.tagName.toUpperCase() === 'INPUT'||
+            node.tagName.toUpperCase() === 'TEXTAREA'){
+                node.value = value;
+            }else{
+                node.setAttribute(key,value);
+            }
+        break;
+        case 'style':
+            node.style.cssText = value;
+        break;
+        default:
+            node.setAttribute(key,value);
+            break;
+    }
+}
+
+// render 方法可以将vnode转换成真实dom.
 function render(eleObj){
-  let el = document.createElement(eleObj.type);
-  for(let key in eleObj.props){
-    // 设置属性的方法
-    setAttr(el,key,eleObj.props[key]);
-  }
+    let el = document.createElement(eleObj.type);
 
-  // 遍历子节点，如果是虚拟dom 继续渲染, 不是则代表文本节点.
-  eleObj.children.forEach(child=>{
-    child = (child instanceof Element)? render(child) : document.createTextNode(child);
-    el.appendChild(child);
-  });
-  return el;
+    for(let key in eleObj.props){
+        // 设置属性的方法
+        setAttr(el,key,eleObj.props[key]);
+    }
+
+    eleObj.children.forEach(child=>{
+        child = (child instanceof Element)?
+        render(child):document.createTextNode(child);
+        el.appendChild(child);
+    });
+
+    return el;
 }
 
-// 如果平级元素有互换问题，那会导致重新渲染.
-// 新增节点也会被重新更新.
-
-// 将元素插入到页面内.
 function renderDom(el,target){
-  target.appendChild(el);
+    target.appendChild(el);
 }
 
-export {createElement,render,Element,renderDom,setAttr};
+export {createElement,render,renderDom};
